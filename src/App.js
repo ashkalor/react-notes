efe4e9089f09edec9f916f2af735679293073719
AshKalor <akhibkalnoor@gmail.com>
2021-06-05 16:39:17 +0530

    Fixed useHttp functionality in App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./components/hooks/useHttp";
 
 function App() {
- 
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-https-e7a0c-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
   };
-};
+
   return (
     <React.Fragment>
       <NewTask onAddTask={taskAddHandler} />
