10b8a63958c1ec4bf8ae1801baf89f6b280e54e0
AshKalor <akhibkalnoor@gmail.com>
2021-06-05 17:24:23 +0530

    Used useHttp custom component in NewTask Component

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,43 +1,30 @@
-import { useState } from "react";
+import useHttp from "../hooks/useHttp";
 
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-https-e7a0c-default-rtdb.firebaseio.com/tasks.json",
-        {
-          method: "POST",
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createTask = (taskText, taskData) => {
+      const generatedId = taskData.name; // firebase-specific => "name" contains generated id
       const createdTask = { id: generatedId, text: taskText };
 
       props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
-  };
+    };
 
+    sendTaskRequest(
+      {
+        url: "https://react-https-e7a0c-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
   return (
     <Section>
       <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
